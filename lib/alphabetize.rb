980e6102b0e261583bb3ddb4033092b1f122ec42
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-26 03:44:57 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -7,7 +7,7 @@ def alphabetize(arr)
   result = arr
   
   result.sort_by! do |str|
-    str_letter_index = [] #create array to push in each str's character into it; so that it will be sorted into the result array using ESPERANTO_ALPHABET
+    str_letter_index = [] #create array to push in each #str's character into it; so that it will be sorted #into the result array using ESPERANTO_ALPHABET
     
     str.each_char do |letter|
       str_letter_index << ESPERANTO_ALPHABET.index(letter)
