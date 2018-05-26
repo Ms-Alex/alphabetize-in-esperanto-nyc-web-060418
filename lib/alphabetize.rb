7e5de9a753f6abd2858e28f67b8345d4735f9417
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-26 03:26:47 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -9,7 +9,7 @@ def alphabetize(arr)
   result.sort_by! do |word|
     temp = []
     word.each_char do |letter|
-      result << ESPERANTO_ALPHABET.index(letter)
+      remp << ESPERANTO_ALPHABET.index(letter)
     end
     temp
   end
